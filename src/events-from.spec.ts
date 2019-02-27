ee6e96f9518bee34c2a2fe7ff7dea3de8eda15df
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-02-27 17:13:14 +0700

    Implement `latestEventsFrom()`
    Produces events from cached event source.

--- src/events-from.spec.ts
+++ src/events-from.spec.ts
@@ -1,10 +1,12 @@
 import { EventEmitter } from './event-emitter';
-import { eventsFrom } from './events-from';
+import { eventsFrom, latestEventsFrom } from './events-from';
 import { EventProducer } from './event-producer';
 import { EventConsumer } from './event-consumer';
 import { EventInterest } from './event-interest';
+import { trackValue, ValueTracker } from './value';
+import Mock = jest.Mock;
 
-describe('produceEvents', () => {
+describe('eventsFrom', () => {
 
   let source: EventEmitter<[string]>;
   let producer: EventProducer<[string]>;
@@ -31,4 +33,37 @@ describe('produceEvents', () => {
     source.notify('event');
     expect(consumer).not.toHaveBeenCalled();
   });
+});
+
+describe('latestEventsFrom', () => {
+
+  let source: ValueTracker<string>;
+  let producer: EventProducer<[string]>;
+  let consumer: Mock & EventConsumer<[string]>;
+  let interest: EventInterest;
+
+  beforeEach(() => {
+    source = trackValue('initial');
+    producer = latestEventsFrom(source);
+    consumer = jest.fn();
+    interest = producer(consumer);
+  });
+
+  it('produces cached event upon consumer registration', () => {
+    expect(consumer).toHaveBeenCalledWith('initial');
+  });
+  it('produces events from the given source', () => {
+
+    const event = 'other';
+
+    source.it = event;
+    expect(consumer).toHaveBeenCalledWith(event);
+  });
+  it('does not produce events once interest lost', () => {
+    interest.off();
+
+    source.it = 'other';
+    expect(consumer).not.toHaveBeenCalledWith('other');
+  });
+
 });
--- src/events-from.ts
+++ src/events-from.ts
@@ -1,11 +1,25 @@
 import { EventProducer } from './event-producer';
 import { EventSource, onEventKey } from './event-source';
+import { afterEventKey, CachedEventSource } from './cached-event-source';
 
 /**
- * Builds a producer of event originated from the given `source`.
+ * Builds a producer of events originated from the given event `source`.
  *
  * @param source A source of events to produce.
+ *
+ * @returns Event producer instance.
  */
 export function eventsFrom<E extends any[], R>(source: EventSource<E, R>): EventProducer<E, R> {
   return EventProducer.of(consumer => source[onEventKey](consumer));
+}
+
+/**
+ * Builds a producer of latest events originated from the given cached event `source`.
+ *
+ * @param source A cached source of events to produce.
+ *
+ * @returns Event producer instance.
+ */
+export function latestEventsFrom<E extends any[], R>(source: CachedEventSource<E, R>): EventProducer<E, R> {
+  return EventProducer.of(consumer => source[afterEventKey](consumer));
 }
