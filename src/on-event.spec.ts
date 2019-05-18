fdc1687e6243869bc8d929f62e833d5cdb950904
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-05-18 14:17:04 +0700

    Implement `onEventFromAny()`
    ...to receive events from any of the given event senders.

--- src/on-event.spec.ts
+++ src/on-event.spec.ts
@@ -2,10 +2,11 @@ import { passIf } from 'call-thru';
 import { EventEmitter } from './event-emitter';
 import { EventInterest, noEventInterest } from './event-interest';
 import { AfterEvent__symbol } from './event-keeper';
+import { EventNotifier } from "./event-notifier";
 import { EventReceiver } from './event-receiver';
 import { EventSender, OnEvent__symbol } from './event-sender';
-import { OnEvent, onEventBy, onEventFrom, onNever } from './on-event';
-import { trackValue } from './value';
+import { OnEvent, onEventBy, onEventFrom, onEventFromAny, onNever } from './on-event';
+import { trackValue, ValueTracker } from './value';
 import Mock = jest.Mock;
 import Mocked = jest.Mocked;
 
@@ -61,6 +62,56 @@ describe('OnEvent', () => {
       };
 
       expect(onEventFrom(keeper)).toBe(keeper[AfterEvent__symbol]);
+    });
+  });
+
+  describe('from any', () => {
+
+    let source1: EventNotifier<[string]>;
+    let source2: EventNotifier<[string]>;
+    let fromAny: OnEvent<[string]>;
+    let mockReceiver: Mock<void, [string]>;
+    let interest: EventInterest;
+
+    beforeEach(() => {
+      source1 = new EventNotifier();
+      source2 = new EventNotifier();
+      fromAny = onEventFromAny(source1, source2);
+      mockReceiver = jest.fn();
+      interest = fromAny(mockReceiver);
+    });
+
+    it('receives events from any source', () => {
+      source1.send('1');
+      expect(mockReceiver).toHaveBeenCalledWith('1');
+      source1.send('2');
+      expect(mockReceiver).toHaveBeenCalledWith('2');
+    });
+    it('does not send any events without sources', () => {
+      expect(onEventFromAny()).toBe(onNever);
+    });
+    it('stops sending events when interest is lost', () => {
+      interest.off();
+      source1.send('1');
+      expect(mockReceiver).not.toHaveBeenCalled();
+    });
+    it('keeps sending events when some of the sources exhausts', () => {
+      source1.done('reason1');
+      source2.send('2');
+      expect(mockReceiver).toHaveBeenCalledWith('2');
+    });
+    it('exhausts when all sources exhaust', () => {
+
+      const mockDone = jest.fn();
+
+      interest.whenDone(mockDone);
+      source1.done('reason1');
+      source2.done('reason2');
+
+      expect(mockDone).toHaveBeenCalledWith('reason2');
+
+      source1.send('3');
+      expect(mockReceiver).not.toHaveBeenCalled();
     });
   });
 
--- src/on-event.ts
+++ src/on-event.ts
@@ -695,6 +695,44 @@ export function onEventFrom<E extends any[]>(senderOrKeeper: EventSender<E> | Ev
  */
 export const onNever: OnEvent<any> = /*#__PURE__*/ onEventBy(() => noEventInterest());
 
+/**
+ * Builds an `OnEvent` registrar of receivers of events sent by any of the given senders of keepers.
+ *
+ * The resulting event sender exhausts as soon as all sources do.
+ *
+ * @typeparam E An event type. This is a list of event receiver parameter types.
+ * @param sources Event senders or keepers the events originated from.
+ *
+ * @returns An `OnEvent` registrar instance.
+ */
+export function onEventFromAny<E extends any[]>(...sources: (EventSender<E> | EventKeeper<E>)[]): OnEvent<E> {
+  if (!sources.length) {
+    return onNever;
+  }
+
+  return onEventBy<E>(receiver => {
+
+    let remained = sources.length;
+    let interests: EventInterest[] = [];
+    const interest = eventInterest(interestLost);
+
+    interests = sources.map(source => onEventFrom(source)(receiver).whenDone(sourceDone));
+
+    return interest;
+
+    function interestLost(reason: any) {
+      interests.forEach(i => i.off(reason));
+    }
+
+    function sourceDone(reason: any) {
+      if (!--remained) {
+        interest.off(reason);
+        interests = [];
+      }
+    }
+  }).share();
+}
+
 function shareInterestTo<E extends any[]>(onEvent: OnEvent<E>): OnEvent<E> {
 
   const shared = new EventNotifier<E>();
