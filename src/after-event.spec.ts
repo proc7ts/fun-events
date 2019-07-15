39341c22d66f7d50b0478060ab9f10038e94484f
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-07-15 15:50:39 +0700

    Add `afterEventFromEach()` function
    Collects events of the same type from array of event keepers.

--- src/after-event.spec.ts
+++ src/after-event.spec.ts
@@ -3,7 +3,7 @@ import {
   AfterEvent,
   afterEventBy,
   afterEventFrom,
-  afterEventFromAll,
+  afterEventFromAll, afterEventFromEach,
   afterEventOf,
   afterEventOr,
   afterNever
@@ -354,6 +354,82 @@ describe('AfterEvent', () => {
       fromAll(mockReceiver);
       expect(mockReceiver).toHaveBeenCalledWith({ source1: ['init'], source2: [1] });
       expect(recurrentReceiver).toHaveBeenCalledWith({ source1: ['recurrent'], source2: [1] });
+    });
+  });
+
+  describe('from each', () => {
+
+    let source1: ValueTracker<string>;
+    let source2: ValueTracker<string>;
+    let fromEach: AfterEvent<[string][]>;
+    let mockReceiver: Mock<void, [string][]>;
+
+    beforeEach(() => {
+      source1 = trackValue('init1');
+      source2 = trackValue('init2');
+      fromEach = afterEventFromEach(source1, source2);
+      mockReceiver = jest.fn();
+    });
+
+    it('sends initial event only once', () => {
+      fromEach(mockReceiver);
+      expect(mockReceiver).toHaveBeenCalledWith(['init1'], ['init2']);
+      expect(mockReceiver).toHaveBeenCalledTimes(1);
+    });
+    it('does not send anything without sources', () => {
+      fromEach(mockReceiver);
+      expect(afterEventFromEach()).toBe(afterNever);
+    });
+    it('sends updates', () => {
+      fromEach(mockReceiver);
+      mockReceiver.mockClear();
+      source1.it = 'update1';
+      expect(mockReceiver).toHaveBeenCalledWith(['update1'], ['init2']);
+      source2.it = 'update2';
+      expect(mockReceiver).toHaveBeenCalledWith(['update1'], ['update2']);
+    });
+    it('stops sending updates when interest is lost', () => {
+
+      const interest = fromEach(mockReceiver);
+
+      mockReceiver.mockClear();
+      interest.off();
+      source1.it = 'update';
+      expect(mockReceiver).not.toHaveBeenCalled();
+    });
+    it('stops sending updates when interest is lost during registration', () => {
+
+      const reason = 'some reason';
+      const stopper = afterEventBy<[string]>(() => {
+
+        const stop = eventInterest();
+
+        stop.off(reason);
+
+        return stop;
+      });
+
+      const mockDone = jest.fn();
+
+      fromEach = afterEventFromEach(stopper, source2);
+      fromEach(mockReceiver).whenDone(mockDone);
+
+      expect(mockReceiver).not.toHaveBeenCalled();
+      expect(mockDone).toHaveBeenCalledWith(reason);
+    });
+    it('sends recurrent event sent during registration to recurrent receiver', () => {
+
+      const recurrentReceiver = jest.fn();
+
+      mockReceiver.mockImplementation(
+          function (this: EventReceiver.Context<[string][]>) {
+            this.afterRecurrent(recurrentReceiver);
+            source1.it = 'recurrent';
+          });
+
+      fromEach(mockReceiver);
+      expect(mockReceiver).toHaveBeenCalledWith(['init1'], ['init2']);
+      expect(recurrentReceiver).toHaveBeenCalledWith(['recurrent'], ['init2']);
     });
   });
 
--- src/after-event.ts
+++ src/after-event.ts
@@ -1213,7 +1213,6 @@ function noEvent(): never {
 export function afterEventFromAll<S extends { readonly [key: string]: EventKeeper<any> }>(sources: S):
     AfterEvent<[{ readonly [K in keyof S]: EventKeeper.Event<S[K]> }]> {
 
-  // Registering source receivers.
   const keys = Object.keys(sources);
 
   if (!keys.length) {
@@ -1254,5 +1253,56 @@ export function afterEventFromAll<S extends { readonly [key: string]: EventKeepe
             .once((...event) => result[key as keyof S] = event));
 
     return [result];
+  }
+}
+
+/**
+ * Builds an `AfterEvent` registrar of receivers of events sent by each of the `sources`.
+ *
+ * @typeparam E A type of events sent by each source.
+ * @param sources An array of source event keepers.
+ *
+ * @returns An event keeper sending events received from each event keeper. Each event item is an event tuple originated
+ * from event keeper under the same index in `sources` array.
+ */
+export function afterEventFromEach<E extends any[]>(...sources: EventKeeper<E>[]): AfterEvent<E[]> {
+  if (!sources.length) {
+    return afterNever;
+  }
+
+  return afterEventOr(registerReceiver, latestEvent).share();
+
+  function registerReceiver(receiver: EventReceiver<E[]>) {
+
+    const notifier = new EventNotifier<E[]>();
+    const interest = notifier.on(receiver);
+    let send: () => void = noop;
+    const result: E[] = [];
+
+    sources.forEach(readFrom);
+
+    if (!interest.done) {
+      send = () => notifier.send(...result);
+    }
+
+    return interest;
+
+    function readFrom(source: EventKeeper<E>, index: number) {
+      interest.needs(source[AfterEvent__symbol]((...event) => {
+        result[index] = event;
+        send();
+      }).needs(interest));
+    }
+  }
+
+  function latestEvent() {
+
+    const result: E[] = [];
+
+    sources.forEach(source =>
+        afterEventFrom(source)
+            .once((...event) => result.push(event)));
+
+    return result;
   }
 }
