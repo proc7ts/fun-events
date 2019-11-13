eeb295af4ebe167be6cc9cf30edfdbbf38a93aeb
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2019-11-13 10:21:50 +0700

    Multi-output rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -4,30 +4,30 @@ import sourcemaps from 'rollup-plugin-sourcemaps';
 import typescript from 'rollup-plugin-typescript2';
 import pkg from './package.json';
 
-export default [
-  makeConfig({ tsconfig: 'tsconfig.module.json', file: pkg.module, format: 'esm' }),
-  makeConfig({ tsconfig: 'tsconfig.main.json', file: pkg.main, format: 'cjs' }),
-];
-
-function makeConfig({ tsconfig, file, format }) {
-  return {
-    plugins: [
-      commonjs(),
-      typescript({
-        typescript: require('typescript'),
-        tsconfig,
-        cacheRoot: 'target/.rts2_cache',
-        useTsconfigDeclarationDir: true,
-      }),
-      nodeResolve(),
-      sourcemaps(),
-    ],
-    input: './src/index.ts',
-    external: Object.keys(pkg.peerDependencies),
-    output: {
-      file,
-      format,
+export default {
+  plugins: [
+    commonjs(),
+    typescript({
+      typescript: require('typescript'),
+      tsconfig: 'tsconfig.main.json',
+      cacheRoot: 'target/.rts2_cache',
+      useTsconfigDeclarationDir: true,
+    }),
+    nodeResolve(),
+    sourcemaps(),
+  ],
+  input: './src/index.ts',
+  external: Object.keys(pkg.peerDependencies),
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
       sourcemap: true,
     },
-  };
-}
+    {
+      file: pkg.module,
+      format: 'esm',
+      sourcemap: true,
+    },
+  ],
+};
