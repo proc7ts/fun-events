bf9b689248098bfacc54778858b1efd3c7d9ef6f
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-01-30 12:07:00 +0700

    Add `onAnyAsync()`

--- src/senders/index.ts
+++ src/senders/index.ts
@@ -3,4 +3,5 @@
  * @module fun-events
  */
 export * from './on-any';
+export * from './on-any-async';
 export * from './on-promise';
---
+++ src/senders/on-any-async.ts
@@ -0,0 +1,48 @@
+/**
+ * @packageDocumentation
+ * @module fun-events
+ */
+import { EventNotifier } from '../event-notifier';
+import { EventSender, OnEvent__symbol } from '../event-sender';
+import { OnEvent, onEventBy } from '../on-event';
+
+/**
+ * Builds an [[OnEvent]] sender of any of asynchronously resolved events originated from the given sender of
+ * unresolved events.
+ *
+ * Receives events or their promises from the given event sender, and sends them once they are resolved. The original
+ * order of events is not preserved. Instead each resolved event is sent along with its index in original order.
+ *
+ * The resulting events supply is cut off immediately once unresolved events supply is cut off, or some of incoming
+ * event promises rejected. In the latter case the rejection reason is used as a reason to cut off.
+ *
+ * @param from  Unresolved events sender containing either events or their promises.
+ *
+ * @returns New `OnEvent` sender of resolved events and their indices in original order starting from `1`.
+ */
+export function onAnyAsync<E>(from: EventSender<[PromiseLike<E> | E]>): OnEvent<[E, number]> {
+  return onEventBy(receiver => {
+
+    const { supply } = receiver;
+    const sender = new EventNotifier<[E, number]>();
+
+    sender.on(receiver);
+
+    let lastIndex = 0;
+
+    from[OnEvent__symbol]({
+      supply,
+      receive(_ctx, promise) {
+
+        const index = ++lastIndex;
+
+        Promise.resolve()
+            .then(() => promise)
+            .then(
+                event => sender.send(event, index),
+                reason => supply.off(reason),
+            );
+      },
+    });
+  });
+}
