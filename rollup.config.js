8ed42c5f046e1204bc27629a48b9f266e113f82f
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-02-16 17:43:46 +0700

    Upgrade deps

--- rollup.config.js
+++ rollup.config.js
@@ -14,7 +14,6 @@ export default {
       tsconfig: 'tsconfig.main.json',
       cacheRoot: 'target/.rts2_cache',
       useTsconfigDeclarationDir: true,
-      objectHashIgnoreUnknownHack: true,
     }),
     nodeResolve(),
     sourcemaps(),
