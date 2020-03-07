27d8e93884cb9c07ff3628578123cc22a5edc3bf
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-03-07 11:46:38 +0700

    Simplify `afterThe()` implementation

--- src/keepers/after-the.ts
+++ src/keepers/after-the.ts
@@ -2,9 +2,8 @@
  * @packageDocumentation
  * @module fun-events
  */
-import { valueProvider } from 'call-thru';
+import { noop, valueProvider } from 'call-thru';
 import { AfterEvent, afterEventBy } from '../after-event';
-import { eventSupply } from '../base';
 
 /**
  * Builds an [[AfterEvent]] keeper of the given `event`.
@@ -15,5 +14,5 @@ import { eventSupply } from '../base';
  * @returns An [[AfterEvent]] keeper that always sends the given `event`.
  */
 export function afterThe<E extends any[]>(...event: E): AfterEvent<E> {
-  return afterEventBy(() => eventSupply(), valueProvider(event));
+  return afterEventBy(noop, valueProvider(event));
 }
