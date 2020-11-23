cdef781922ad08e51fb41a61b1f75706790a7709
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-11-23 17:17:50 +0700

    Generate type definitions with `@proc7ts/rollup-plugin-flat-dts`

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import { externalModules } from '@proc7ts/rollup-helpers';
+import flatDts from '@proc7ts/rollup-plugin-flat-dts';
 import commonjs from '@rollup/plugin-commonjs';
 import nodeResolve from '@rollup/plugin-node-resolve';
 import path from 'path';
@@ -17,7 +18,6 @@ export default {
       typescript,
       tsconfig: 'tsconfig.main.json',
       cacheRoot: 'target/.rts2_cache',
-      useTsconfigDeclarationDir: true,
     }),
     nodeResolve(),
     sourcemaps(),
@@ -47,10 +47,16 @@ export default {
     {
       format: 'esm',
       sourcemap: true,
-      dir: './dist',
-      entryFileNames: '[name].js',
-      chunkFileNames: '_[name].js',
+      dir: '.',
+      entryFileNames: 'dist/[name].js',
+      chunkFileNames: 'dist/_[name].js',
       hoistTransitiveImports: false,
+      plugins: [
+        flatDts({
+          tsconfig: 'tsconfig.main.json',
+          entries: ['dom'],
+        }),
+      ],
     },
   ],
 };
