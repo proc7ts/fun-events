655310beabb806a2c42882032baff78200726ca6
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2020-12-15 14:41:56 +0700

    `translateAfter`: Add fallback support

--- src/processors/translate-after.spec.ts
+++ src/processors/translate-after.spec.ts
@@ -1,5 +1,6 @@
 import { valuesProvider } from '@proc7ts/primitives';
 import { AfterEvent } from '../after-event';
+import { EventEmitter } from '../senders';
 import { trackValue } from '../value';
 import { translateAfter } from './translate-after';
 
@@ -18,6 +19,19 @@ describe('translateAfter', () => {
     tracker.it = [2, 22];
     expect(receiver).toHaveBeenLastCalledWith(22, 2);
 
+    expect(receiver).toHaveBeenCalledTimes(2);
+  });
+  it('translates events with fallback', () => {
+
+    const emitter = new EventEmitter<[number, number]>();
+    const onEvent = emitter.on.do(translateAfter((send, a: number, b: number) => send(b, a), valuesProvider(0, -1)));
+    const receiver = jest.fn<void, [number, number]>();
+
+    onEvent(receiver);
+
+    emitter.send(2, 11);
+    expect(receiver).toHaveBeenCalledWith(0, -1);
+    expect(receiver).toHaveBeenLastCalledWith(11, 2);
     expect(receiver).toHaveBeenCalledTimes(2);
   });
   it('translates some events', () => {
--- src/processors/translate-after.ts
+++ src/processors/translate-after.ts
@@ -4,6 +4,7 @@
  */
 import { AfterEvent, afterEventBy } from '../after-event';
 import { translateEvents } from '../impl';
+import { OnEvent } from '../on-event';
 import { shareAfter } from './share-after';
 
 /**
@@ -20,8 +21,6 @@ import { shareAfter } from './share-after';
  * @typeParam TOutEvent - Outgoing translated event type.
  * @param translate - Event translation function. Accepts an outgoing event sender function as first parameter,
  * and incoming event as the rest of them.
- * @param fallback - A function creating fallback event. When omitted, the initial event is expected to be sent by
- * `translate` function. A receiver registration would lead to an error otherwise.
  *
  * @returns A mapping function of incoming event keeper to another one.
  */
@@ -30,10 +29,43 @@ export function translateAfter<
     TOutEvent extends any[],
     >(
     translate: (this: void, send: (...event: TOutEvent) => void, ...event: TInEvent) => void,
-    fallback?: (this: void) => TOutEvent,
-): (this: void, input: AfterEvent<TInEvent>) => AfterEvent<TOutEvent> {
+): (this: void, input: AfterEvent<TInEvent>) => AfterEvent<TOutEvent>;
 
-  const mapper = translateAfter_(translate, fallback);
+/**
+ * Creates an event processor that translates events incoming from {@link OnEvent} sender with fallback.
+ *
+ * The translated events expected to be sent by the given `translate` function.
+ *
+ * The returned mapper does the same as the one created by {@link translateOn} one, but returns an {@link AfterEvent}
+ * keeper instead of {@link OnEvent} sender. This can not be always done without a `fallback`, as not every
+ * transformation results to valid {@link EventKeeper}. E.g. when some events filtered out.
+ *
+ * @category Core
+ * @typeParam TInEvent - Incoming event type.
+ * @typeParam TOutEvent - Outgoing translated event type.
+ * @param translate - Event translation function. Accepts an outgoing event sender function as first parameter,
+ * and incoming event as the rest of them.
+ * @param fallback - A function creating outgoing event fallback.
+ *
+ * @returns A mapping function of incoming event keeper to another one.
+ */
+export function translateAfter<
+    TInEvent extends any[],
+    TOutEvent extends any[],
+    >(
+    translate: (this: void, send: (...event: TOutEvent) => void, ...event: TInEvent) => void,
+    fallback: (this: void) => TOutEvent,
+): (this: void, input: OnEvent<TInEvent>) => AfterEvent<TOutEvent>;
+
+export function translateAfter<
+    TInEvent extends any[],
+    TOutEvent extends any[],
+    >(
+    translate: (this: void, send: (...event: TOutEvent) => void, ...event: TInEvent) => void,
+    fallback?: (this: void) => TOutEvent,
+): (this: void, input: OnEvent<TInEvent>) => AfterEvent<TOutEvent> {
+
+  const mapper = translateAfter_(translate, fallback!);
 
   return input => shareAfter(mapper(input));
 }
@@ -41,6 +73,58 @@ export function translateAfter<
 /**
  * Creates an event processor that translates events incoming from {@link AfterEvent} keeper, and does not share the
  * outgoing events supply.
+ *
+ * The translated events expected to be sent by the given `translate` function.
+ *
+ * The returned mapper does the same as the one created by {@link translateOn_} one, but returns an {@link AfterEvent}
+ * keeper instead of {@link OnEvent} sender. This can not be always done without a `fallback`, as not every
+ * transformation results to valid {@link EventKeeper}. E.g. when some events filtered out.
+ *
+ * @category Core
+ * @typeParam TInEvent - Incoming event type.
+ * @typeParam TOutEvent - Outgoing translated event type.
+ * @param translate - Event translation function. Accepts an outgoing event sender function as first parameter,
+ * and incoming event as the rest of them.
+ *
+ * @returns A mapping function of incoming event keeper to another one.
+ */
+export function translateAfter_<// eslint-disable-line @typescript-eslint/naming-convention
+    TInEvent extends any[],
+    TOutEvent extends any[],
+    >(
+    translate: (this: void, send: (...event: TOutEvent) => void, ...event: TInEvent) => void,
+): (this: void, input: AfterEvent<TInEvent>) => AfterEvent<TOutEvent>;
+
+/**
+ * Creates an event processor that translates events incoming from {@link OnEvent} sender with fallback, and does not
+ * share the outgoing events supply.
+ *
+ * The translated events expected to be sent by the given `translate` function.
+ *
+ * The returned mapper does the same as the one created by {@link translateOn_} one, but returns an {@link AfterEvent}
+ * keeper instead of {@link OnEvent} sender. This can not be always done without a `fallback`, as not every
+ * transformation results to valid {@link EventKeeper}. E.g. when some events filtered out.
+ *
+ * @category Core
+ * @typeParam TInEvent - Incoming event type.
+ * @typeParam TOutEvent - Outgoing translated event type.
+ * @param translate - Event translation function. Accepts an outgoing event sender function as first parameter,
+ * and incoming event as the rest of them.
+ * @param fallback - A function creating outgoing event fallback.
+ *
+ * @returns A mapping function of incoming event keeper to another one.
+ */
+export function translateAfter_<// eslint-disable-line @typescript-eslint/naming-convention
+    TInEvent extends any[],
+    TOutEvent extends any[],
+    >(
+    translate: (this: void, send: (...event: TOutEvent) => void, ...event: TInEvent) => void,
+    fallback: (this: void) => TOutEvent,
+): (this: void, input: OnEvent<TInEvent>) => AfterEvent<TOutEvent>;
+
+/**
+ * Creates an event processor that translates events incoming from {@link OnEvent} sender with fallback, and does not
+ * share the outgoing events supply.
  *
  * The translated events expected to be sent by the given `translate` function.
  *
@@ -64,6 +148,6 @@ export function translateAfter_<// eslint-disable-line @typescript-eslint/naming
     >(
     translate: (this: void, send: (...event: TOutEvent) => void, ...event: TInEvent) => void,
     fallback?: (this: void) => TOutEvent,
-): (this: void, input: AfterEvent<TInEvent>) => AfterEvent<TOutEvent> {
+): (this: void, input: OnEvent<TInEvent>) => AfterEvent<TOutEvent> {
   return input => afterEventBy(translateEvents(input, translate), fallback);
 }
