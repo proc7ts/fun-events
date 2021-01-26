cf8912a65ff8fba35f4aa9a240660c38a417aafc
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-01-26 19:00:50 +0700

    Improve event sharing

--- src/processors/share-on.spec.ts
+++ src/processors/share-on.spec.ts
@@ -1,3 +1,4 @@
+import { asis } from '@proc7ts/primitives';
 import { AfterEvent, afterEventBy } from '../after-event';
 import { EventNotifier, EventReceiver } from '../base';
 import { OnEvent, onEventBy } from '../on-event';
@@ -174,6 +175,13 @@ describe('shareOn', () => {
       emitter.send('a', 'b');
       expect(mockReceiver).toHaveBeenCalledWith('a', 'b');
       expect(mockReceiver2).toHaveBeenCalledWith('a', 'b');
+    });
+    it('handles immediate source supply cut off', async () => {
+      mockRegister.mockImplementation(({ supply }) => supply.off('reason'));
+
+      const shared = afterEvent.do(shareOn);
+
+      expect(await shared(mockReceiver).whenDone().catch(asis)).toBe('reason');
     });
   });
 });
