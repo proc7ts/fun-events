b5eef24dbb1ad71def84fbb77d397c4fe9d4fefb
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2021-02-28 16:39:59 +0700

    Improve `afterValue()` signature to reflect `undefined` values

--- src/keepers/after-value.ts
+++ src/keepers/after-value.ts
@@ -10,8 +10,23 @@ import { afterThe } from './after-the';
  * of such values.
  *
  * @returns An {@link AfterEvent} keeper that always sends the given `value`, or the `value` itself, when it is already
- * and `AfterEvent` keeper.
+ * an `AfterEvent` keeper.
  */
+export function afterValue<T>(value: T | AfterEvent<[T]>): AfterEvent<[T]>;
+
+/**
+ * Builds an {@link AfterEvent} keeper of the optional `value`.
+ *
+ * @category Core
+ * @typeParam T - Value type.
+ * @param value - Either `undefined`, a value that will be sent to all receivers upon registration, or an
+ * {@link AfterEvent} keeper of optional values.
+ *
+ * @returns An {@link AfterEvent} keeper that always sends the given `value`, or the `value` itself, when it is already
+ * an `AfterEvent` keeper.
+ */
+export function afterValue<T>(value: T | AfterEvent<[T?]> | undefined): AfterEvent<[T?]>;
+
 export function afterValue<T>(value: T | AfterEvent<[T]>): AfterEvent<[T]> {
   return isAfterEvent(value) ? value : afterThe(value);
 }
