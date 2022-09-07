6cff6515134b70321bfcc923f86941ed560eb94e
Ruslan Lopatin <ruslan.lopatin@gmail.com>
2022-09-07 12:14:48 +0700

    Upgrade Jest config to new format

--- jest.config.js
+++ jest.config.js
@@ -32,10 +32,13 @@ export default {
     ],
   ],
   testEnvironment: 'node',
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tsconfig.spec.json',
-      useESM: true,
-    },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.spec.json',
+        useESM: true,
+      },
+    ],
   },
 };
